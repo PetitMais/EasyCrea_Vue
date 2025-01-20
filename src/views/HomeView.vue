7a9b0bc10a1e6ebae937ebff7a80bfc4b3a1aa87
LShiftt <Lshiftprog@gmail.com>
2025-01-20 18:44:09 +0100

    fix: new url

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import ChoiceHome from './../components/ChoiceHome.vue';
 </script>
 
 <template>
-    <a href="https://expo.dev/accounts/twist404/projects/deckouvert/builds/07ccd979-3593-493f-a8d6-b7f023ecaaaa" target="_blank">Deckouverte</a>
+    <a href="https://expo.dev/accounts/twist404/projects/deckouvert/builds/aa4d12a8-7e10-4863-a297-528da267164e" target="_blank">Deckouverte</a>
   <main>
     <ChoiceHome/>
   </main>
